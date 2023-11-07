48994bfe4efa0909497ba5e08a95c5d2d9c0cc46
ChimneyFish <115329981+ChimneyFish@users.noreply.github.com>
2023-11-07 00:52:53 -0800

    Add files via upload

---
+++ Linux-swap-file.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+
+# Function to display available drives
+display_drives() {
+  echo "Available drives:"
+  lsblk -o NAME,SIZE,TYPE,MOUNTPOINT | grep -E 'disk *$'
+}
+
+# Function to create a swap partition
+create_swap_partition() {
+  local drive="$1"
+  local swap_size="$2"
+
+  # Check if the drive exists
+  if [ ! -e "/dev/$drive" ]; then
+    echo "Drive /dev/$drive does not exist."
+    return 1
+  fi
+
+  # Create the swap partition
+  echo "Creating swap partition on /dev/$drive"
+  sudo parted /dev/"$drive" mklabel gpt
+  sudo parted /dev/"$drive" mkpart primary linux-swap 0% "$swap_size"
+  sudo mkswap /dev/"$drive"1
+  sudo swapon /dev/"$drive"1
+
+  # Add entry to /etc/fstab
+  echo "/dev/$drive"1 none swap sw 0 0 | sudo tee -a /etc/fstab
+
+  echo "Swap partition created and activated on /dev/$drive."
+}
+
+# Display available drives
+display_drives
+
+# Ask the user to select a drive
+read -p "Enter the drive where you want to create the swap partition (e.g., sda, nvme0n1): " selected_drive
+
+# Ask for the swap size
+read -p "Enter the swap size (e.g., 2G, 4G): " swap_size
+
+# Create the swap partition
+create_swap_partition "$selected_drive" "$swap_size"
+
+echo "Swap configuration complete."
